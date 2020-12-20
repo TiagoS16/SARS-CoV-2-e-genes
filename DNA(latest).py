4e621a847e069fb497c92a09ad5a9f27b9474751
Tiago Machado <Tiagom_97>
2020-12-20 22:54:01 +0000

    Funcoes documentadas e com função para gerar txt com tabela do blast

--- DNA(latest).py
+++ DNA(latest).py
@@ -4,9 +4,13 @@ from Bio import SearchIO
 from Bio.Blast import NCBIXML
 from Bio.Blast import NCBIWWW
 
-def bio_dna(ficheiro):
+def DNA_inf(ficheiro):
     '''
-    ficheiro = nome para ficheiro de extensão ponto gb (ex: prot.gb) ;
+    VARIAVEIS:
+        ficheiro = nome para ficheiro de extensão ponto gb (ex: prot.gb)
+        type - tipo de ficheiro a ser lido (EX. GENBANK)
+    RETURNS:
+        Imprime informações contidas no ficheiro
     '''
     record = SeqIO.read(ficheiro, 'genbank')
     id = record.name
@@ -18,16 +22,30 @@ def bio_dna(ficheiro):
         print(i)
 
 def blast_DNA(genbank, blast_file):
+    '''
+    VARIAVEIS:
+        genbank = ficheiro genbank com a extensão ponto gb (ex: prot.gb)
+        blast_file = Nome do ficheiro a dar com o resultado do Blast ( ex: "FGB_blast.xml")
+    RETURNS:
+        Gera um ficheiro .xml com o resultado do blastn contra   a database dos nucleotidos .
+    '''
     record = SeqIO.read(genbank, format='gb')
     result_handle = NCBIWWW.qblast('blastn', 'nt', record.seq)
     with open(blast_file, "w") as out_handle:
         out_handle.write(result_handle.read())
     result_handle.close()
 
-def isol_AC(x, id):
+def isol_AC(lista, id):
+    '''
+    VARIAVEIS:
+        id = id associado ao gene a ser tratado
+        lista = Lista devolvida filtrada pela função obter_x
+    RETURNS:
+        Lista de Acession numbers filtrados pelo tipo gb e sem repetições.
+    '''
     DicAC = {}
     DicAC[id] = 1
-    for hit in x:
+    for hit in lista:
         c = []
         for y in range(len(hit)):
             if hit[y] == '|':
@@ -40,7 +58,16 @@ def isol_AC(x, id):
     ListAC = list(DicAC.keys())
     return ListAC
 
-def obter_x(file, E_VALUE_THRESH):
+def obter_lista(file, E_VALUE_THRESH):
+    '''
+    VARIAVEIS:
+        file = Nome do ficheiro a dar com o resultado do Blast ( ex: "FGB_blast.xml")
+        E_VALUE_THRESH = Recebe valor None ou qualquer numero inteiro, corresponde ao valor de e-value maximo
+        aceitavel para tratamento do outpur do blast.
+    RETURNS:
+        Devolve uma lista dos resultados do blast filtrados pelo e-value, com o nome do alinhamento, comprimento
+        do alinhamento e e-value
+    '''
     result_handle = open(file)
     blast_record = NCBIXML.read(result_handle)
     FILE = []
@@ -55,18 +82,62 @@ def obter_x(file, E_VALUE_THRESH):
     return FILE
 
 def DNA(genbank, id, file,blast = False, E_VALUE_THRESH = None):
-    bio_dna(genbank)
+    '''
+    VARIAVEIS:
+        genbank = ficheiro genbank com a extensão ponto gb (ex: prot.gb)
+        id = id associado ao gene a ser tratado
+        file = Nome do ficheiro a dar com o resultado do Blast ( ex: "FGB_blast.xml")
+        blast = Em caso de omissão recebe valor  de False, se Blast = True irá utilizar o ficheiro genbank e
+        realizar um blast.
+        E_VALUE_THRESH = Recebe valor None ou qualquer numero inteiro, corresponde ao valor de e-value maximo
+        aceitavel para tratamento do outpur do blast.
+    RETURNS:
+        Gera um ficheiro .txt contendo os Acession Number dos resultados do blast com filtração segundo o
+        E_VALUE_THRESH e "gb".
+    '''
+
+    DNA_inf(genbank)
     if blast == True:
         blast_DNA(genbank, file)
-    x = obter_x(file, E_VALUE_THRESH)
+    x = obter_lista(file, E_VALUE_THRESH)
     ListAC = isol_AC(x, id)
     with open('id_list_DNA.txt', 'w') as f:
         for item in ListAC:
             f.write("%s\n" % item)
 
-DNA('FGG.gb','NC_000004.12', 'FGG_blast.xml', False, None)
+
+def isol_blasthit(x):
+    '''
+    VARIAVEIS:
+        x = Lista devolvida filtrada pela função obter_lista
+    RETURNS:
+        Ficheiro contento todos os resultados do Blast com o respetivo Acession Number | tipo | e descrição do resultado.
+    '''
+    DicAC = {}
+    for hit in x:
+        print(hit)
+        c = []
+        for y in range(len(hit)):
+            if hit[y] == '|':
+                c.append(y)
+        type = hit[0 : c[0]]
+        y = hit[c[0] + 1: c[1]]
+        descrip = hit[c[1] + 1: c[2]]
+        final = y + ' | ' + type + ' | ' + descrip
+        if final not in DicAC:
+            DicAC[final] = 1
+    ListAC = list(DicAC.keys())
+    with open('blast_qresult.txt', 'w') as f:
+        for i in ListAC:
+            f.write("%s\n" % i)
 
 def parse_dna(file):
+    '''
+    VARIAVEIS:
+        file = ficheiro xml contendo os resultados de um blast
+    RETURNS:
+        Imprime na consola informações relativas a esse blast
+    '''
     result_handle = open(file)
     from Bio.Blast import NCBIXML
     blast_record = NCBIXML.read(result_handle)
@@ -85,5 +156,3 @@ def parse_dna(file):
     blast_qresult = SearchIO.read(file, "blast-xml")
     print(blast_qresult)
     result_handle.close()
-
-parse_dna('FGG_blast.xml')
