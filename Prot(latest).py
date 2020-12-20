e3e9db55e5f571391fe737df9b87c2ba293f217d
Tiago Machado <Tiagom_97>
2020-12-20 23:32:18 +0000

    Funcoes das prot documentadas e com função para gerar txt com tabela do blast

--- Prot(latest).py
+++ Prot(latest).py
@@ -8,7 +8,10 @@ from Bio import SearchIO
 
 def get_prot(id):
     '''
-    id= codigo da proteina a pesquisar na base de dados Swiss-Prot;
+    VARIAVEIS:
+        id= codigo da proteina a pesquisar na base de dados Swiss-Prot;
+    RETURNS:
+        Lista de Acession numbers filtrados pelo tipo gb e sem repetições
     '''
     from Bio import ExPASy
     with ExPASy.get_sprot_raw(id) as handle:
@@ -26,16 +29,39 @@ def get_prot(id):
     return y
 
 def filtro(seq):
+    '''
+    VARIAVEIS:
+        seq = string resultado do get_prot(id)
+    RETURNS:
+        Lista da string separada pelas "|"
+    '''
     seq = seq.split('|')
     return seq
 
 def blast_prot(FILE, seq):
+    '''
+    VARIAVEIS:
+        FILE = Nome do ficheiro a dar com o resultado do Blast ( ex: "FGB_blast.xml")
+        seq - sequencia proteica query para executar o blast
+    RETURNS:
+        Devolve uma lista dos resultados do blast filtrados pelo e-value, com o nome do alinhamento, comprimento
+        do alinhamento e e-value
+    '''
     result_handle = NCBIWWW.qblast('blastp', 'nr', seq)
     with open(FILE, "w") as out_handle:
         out_handle.write(result_handle.read())
     result_handle.close()
 
 def parse(file, E_VALUE_THRESH):
+    '''
+    VARIAVEIS:
+        file = Nome do ficheiro a dar com o resultado do Blast ( ex: "FGB_blast.xml")
+        E_VALUE_THRESH = Recebe valor None ou qualquer numero inteiro, corresponde ao valor de e-value maximo
+        aceitável para tratamento do output do blast.
+    RETURNS:
+        Devolve uma lista dos resultados do blast filtrados pelo e-value, com o nome do alinhamento, comprimento
+        do alinhamento e e-value
+    '''
     result_handle = open(file)
     blast_record = NCBIXML.read(result_handle)
     FILE = []
@@ -48,9 +74,15 @@ def parse(file, E_VALUE_THRESH):
                 FILE.append(y)
     return FILE
 
-def isol_AC(x):
+def isol_AC(parsed_list):
+    '''
+    VARIAVEIS:
+        parsed_list = Lista devolvida filtrada pela função parse
+    RETURNS:
+        Lista de Acession numbers filtrados pelo tipo gb e sem repetições.
+    '''
     DicAC = {}
-    for hit in x:
+    for hit in parsed_list:
         c = []
         for y in range(len(hit)):
             if hit[y] == '|':
@@ -64,6 +96,19 @@ def isol_AC(x):
     return ListAC
 
 def proteico(id,file,blast = False, E_VALUE_THRESH = None):
+    '''
+    VARIAVEIS:
+        genbank = ficheiro genbank com a extensão ponto gb (ex: prot.gb)
+        id = id da base de SWISS Prot da proteina a ser tratada
+        file = Nome do ficheiro a dar com o resultado do Blast ( ex: "FGB_blast.xml")
+        blast = Em caso de omissão recebe valor  de False, se Blast = True irá utilizar o ficheiro genbank e
+        realizar um blast.
+        E_VALUE_THRESH = Recebe valor None ou qualquer numero inteiro, corresponde ao valor de e-value maximo
+        aceitavel para tratamento do output do blast.
+    RETURNS:
+        Gera um ficheiro .txt contendo os Acession Number dos resultados do blast com filtração segundo o
+        E_VALUE_THRESH e "gb".
+        '''
     if blast == True:
         x = get_prot(id)
         print(x)
@@ -80,10 +125,13 @@ def proteico(id,file,blast = False, E_VALUE_THRESH = None):
         for item in ListAC:
             f.write("%s\n" % item)
 
-proteico("P02679","P02679_blast.xml" ,False, None)
-
-
 def parse_prot(file):
+    '''
+    VARIAVEIS:
+        file = Nome do ficheiro resultado do Blast ( ex: "FGB_blast.xml")
+    RETURNS:
+        Informações contidas no ficheiro impressos sobre na consola.
+    '''
     result_handle = open(file)
     from Bio.Blast import NCBIXML
     blast_record = NCBIXML.read(result_handle)
@@ -104,14 +152,29 @@ def parse_prot(file):
     result_handle.close()
 
 
-parse_prot("P02679_blast.xml")
 
-
-# CHAMADAS
-# bio_prot('ORF3a.gb', 'genbank')
-# blast_prot('ORF3a.gb', 'gb', 'ORF3a_blast.xml')
-# blast_prot_pars('ORF3a_blast.xml')
-# get_prot('P02675', True)
-#blast_prot('P0DTC3', "PROT_ORF3a_blast1.xml")
-#blast_prot_pars("PROT_ORF3a_blast.xml")
+def isol_blasthit(lista):
+    '''
+    VARIAVEIS:
+        lista = Lista devolvida filtrada pela função parse
+    RETURNS:
+        Ficheiro contento todos os resultados do Blast com o respetivo Acession Number | tipo | e descrição do resultado.
+    '''
+    DicAC = {}
+    for hit in lista:
+        print(hit)
+        c = []
+        for y in range(len(hit)):
+            if hit[y] == '|':
+                c.append(y)
+        type = hit[0 : c[0]]
+        y = hit[c[0] + 1: c[1]]
+        descrip = hit[c[1] + 1: c[2]]
+        final = y + ' | ' + type + ' | ' + descrip
+        if final not in DicAC:
+            DicAC[final] = 1
+    ListAC = list(DicAC.keys())
+    with open('blast_qresult.txt', 'w') as f:
+        for i in ListAC:
+            f.write("%s\n" % i)
 
