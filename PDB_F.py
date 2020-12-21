7ed2df0d3b8494b6db57281327316967d9a03068
pg428 <74025156+pg428@users.noreply.github.com>
2020-12-21 00:07:18 +0000

    Add files via upload

---
+++ PDB_F.py
@@ -0,0 +1,19 @@
+from Bio.PDB import *
+import pymol
+def PDB(ID):Re
+    """"
+    ID: fornecer o ID da PDB
+    Return: A estrutura 3D da proteína
+    """
+    pdbl = PDBList()
+    file = pdbl.retrieve_pdb_file(ID, pdir='.', file_format='pdb')
+    pymol.finish_launching()
+
+    pymol.cmd.load(file, ID)
+    pymol.cmd.disable("all")
+    pymol.cmd.enable(ID)
+    pymol.cmd.zoom()
+    pymol.cmd.png("Protein2.png", 3000, 3000, dpi=500, ray=1)
+
+    pymol.cmd.quit()
+
