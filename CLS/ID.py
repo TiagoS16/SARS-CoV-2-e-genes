187abcabfb2ca4fa1392ff9db4f1bf9102ad9c47
migue <pg42877@alunos.uminho.pt>
2021-01-02 17:45:04 +0000

    id working

--- CLS/ID.py
+++ CLS/ID.py
@@ -2,8 +2,9 @@ from Bio import Entrez
 from Bio import SeqIO
 
 class DNA_ID:
-    def __init__(self, query, database, res, email, file_output):
+    def __init__(self, query, Orgn, database, res, email, file_output):
         self.query = query
+        self.Orgn = Orgn
         self.database = database
         self.resultados = res
         self.email = email
@@ -11,20 +12,23 @@ class DNA_ID:
 
     def DNA(self):
         Entrez.email = self.email
-        handle = Entrez.esearch(db=self.database, sort='revelance', term='Homo Sapiens[Orgn] AND '+ self.query + '[Gene]', retmax=self.resultados)
+        handle = Entrez.esearch(db=self.database, sort='relevance', term= self.Orgn + self.query + ', RefSeq', retmax=self.resultados)
+        #Refseq para ORF3, corona virus 2
+        #RefSeqGene para FGA,FGB,FGG
         record = Entrez.read(handle)
         idlist = record['IdList']
+        print(idlist)
         handle.close()
         return idlist[0]
 
     def Save_file(self):
         id_gene = DNA_ID.DNA(self)
-        File = Entrez.efetch(db="gene", id=id_gene, retmode='xml', rettype=self.file_output)
-        read = SeqIO.read(file,self.file_output)
+        File = Entrez.efetch(db=self.database, id=id_gene, retmode='text', rettype=self.file_output)
+        read = SeqIO.read(File,self.file_output)
         File.close()
-        name = id_gene + "_gene." + self.file_output
+        name = self.query + "_" + self.database + "_" + self.file_output + ".txt"
+        print(name)
         SeqIO.write(read, name ,self.file_output)
-
 
 
 class Prot_ID:
@@ -34,14 +38,14 @@ class Prot_ID:
     RETURNS:
        Lista de Acession numbers filtrados pelo tipo gb e sem repetições
     '''
+    database = 'swiss'
     def __init__(self, id):
         self.id = id
-        self.database = 'swiss'
 
     def get_prot(self):
         from Bio import ExPASy
         with ExPASy.get_sprot_raw(self.id) as handle:
-            seq_record = SeqIO.read(handle, self.database)
+            seq_record = SeqIO.read(handle, Prot_ID.database)
         tam = len(seq_record.seq)
         seq = seq_record.seq
         tax = seq_record.annotations["taxonomy"]
@@ -50,3 +54,12 @@ class Prot_ID:
         file = open(name, 'w+')
         file.write('> ID:' + self.id + '_'+ str(tam) + 'bp' + '_' +  str(tax) + '_' + org + '\n' + seq + '\n')
         return seq
+
+teste = DNA_ID('ORF3a','SARS-CoV-2','protein',10,'pg42877@alunos,uminho.pt','fasta')
+DNA_ID.Save_file(teste)
+
+
+# db = 'protein', para seqs de proteina de FGA,FGB,FGG; ORGN = Homo Sapiens [Orgn]
+# db = 'protein', para seqs de proteina de ORF3a; ORGN = Coronavirus 2
+# db = 'Nucleotide' para seqs de DNA de FGA,FGB,FGG; ORGN = Homo Sapiens [Orgn]
+# db = 'Nucleotide' para seqs de DNA da ORF3a; ORGN = Corona virus 2
