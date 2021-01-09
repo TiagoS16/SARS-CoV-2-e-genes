aa93e20160de999c856873d08fb858a230bbc106
Tiago Moreira Machado <74023916+Tel-las@users.noreply.github.com>
2021-01-09 15:40:42 +0000

    Update

--- Multiple_Alignment.py
+++ Multiple_Alignment.py
@@ -1,7 +1,7 @@
 from Bio.Align.Applications import ClustalwCommandline
 
 diretoria = r'C:\Program Files (x86)\ClustalW2\clustalw2.exe'
-in_file = r'SeqsHomologas_P02675_blast.xml.fasta'
+in_file = r'entrez_prot.fasta'
 
 clustalw_cline = ClustalwCommandline(diretoria, infile=in_file)
 clustalw_cline()
